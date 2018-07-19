6369f494cfa327361ec95cb489d36395a942a026
Théo Dury <theo@theos-macbook-pro.home>
2018-07-19 23:16:32 +0200

    add first link in the cities page

--- src/app/cities/cities.module.ts
+++ src/app/cities/cities.module.ts
@@ -4,10 +4,14 @@ import { CommonModule } from '@angular/common';
 import { CitiesRoutingModule } from './cities-routing.module';
 import { CitiesComponent } from './cities.component';
 
+import {MatButtonModule} from '@angular/material/button';
+
+
 @NgModule({
   imports: [
     CommonModule,
-    CitiesRoutingModule
+    CitiesRoutingModule,
+    MatButtonModule
   ],
   declarations: [CitiesComponent]
 })
